12a21d1f90858c5f949b529e6b8883c3b62014cb
Bro-Montana <97376411+Bro-Montana@users.noreply.github.com>
2022-10-24 13:37:22 -0700

    added components and packages

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import axios from "axios";
 
+
+import { LoginView } from "../login-view/login-view";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+
+const { Button } = '../components/button/button'
 
 export class MainView extends React.Component {
 
@@ -26,15 +30,32 @@ export class MainView extends React.Component {
       });
   }
 
+  /*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` 
+  *property to that movie*/
+
   setSelectedMovie(newSelectedMovie) {
     this.setState({
       selectedMovie: newSelectedMovie
     });
   }
 
-  render() {
-    const { movies, selectedMovie } = this.state;
+  /* When a user successfully logs in, this function updates the `user` property in state to that 
+  *particular user*/
 
+  onLoggedIn(user) {
+    this.setState([
+      user
+    ]);
+  }
+
+  render() {
+    const { movies, selectedMovie, user } = this.state;
+
+    /* If there is no user, the LoginView is rendered. 
+    If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
+    //before movies are loaded
     if (movies.length === 0) return <div className="main-view" />;
 
     return (
@@ -45,6 +66,8 @@ export class MainView extends React.Component {
             <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
           ))
         }
+        <Button label="Click Me" />
+
       </div>
     );
   }
